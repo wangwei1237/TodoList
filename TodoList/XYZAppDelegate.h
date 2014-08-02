dd02ce14e0cab3665b5115b2e34fc747c75453bb
wangwei <wangwei@wangwei.local>
2014-08-02 23:00:03 +0800

    Initial Commit

---
+++ TodoList/XYZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XYZAppDelegate.h
+//  TodoList
+//
+//  Created by wangwei on 14-8-2.
+//  Copyright (c) 2014年 example. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TodoList/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TodoList
+//
+//  Created by wangwei on 14-8-2.
+//  Copyright (c) 2014年 example. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "XYZAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([XYZAppDelegate class]));
+    }
+}
